7a7579bd3aa20c51e94c7606dedd676ee9bdde25
Fernando Falci <falci@falci.me>
2024-02-21 07:19:32 +0100

    fix: don't reuse the https.Agent

--- index.js
+++ index.js
@@ -1,62 +1,61 @@
-const https = require('https')
-const { DANEAgent, setServers, lookup, dns } = require('https-dane')
-
-const agent = new DANEAgent()
+const https = require('https');
+const { DANEAgent, setServers, lookup, dns } = require('https-dane');
 
 const defaultOpts = {
   token: 'HNS',
   maxLength: 90,
-  validate: key => !!key && key.slice(0,3) === 'hs1' && key.length <= 90 // https://wiki.trezor.io/Bech32
-}
+  validate: (key) => !!key && key.slice(0, 3) === 'hs1' && key.length <= 90, // https://wiki.trezor.io/Bech32
+};
 
-function fetchAddress (alias, opts=defaultOpts) {
-  const { token, maxLength, validate } = opts || {}
+function fetchAddress(alias, opts = defaultOpts) {
+  const { token, maxLength, validate } = opts || {};
+  const agent = new DANEAgent();
   return new Promise((resolve, reject) => {
     if (validate(alias)) {
-      const error = new Error('alias cannot be a valid address')
-      error.code = 'ECOLLISION'
-      return reject(error)
+      const error = new Error('alias cannot be a valid address');
+      error.code = 'ECOLLISION';
+      return reject(error);
     }
-    
-    const url = `https://${alias}/.well-known/wallets/${token.toUpperCase()}`
-    const req = https.get(url, { agent, lookup }, res => {
-      let data = ''
-      res.setEncoding('utf8')
-      res.on('data', chunk => {
-        const newLine = chunk.indexOf('\n')
+
+    const url = `https://${alias}/.well-known/wallets/${token.toUpperCase()}`;
+    const req = https.get(url, { agent, lookup }, (res) => {
+      let data = '';
+      res.setEncoding('utf8');
+      res.on('data', (chunk) => {
+        const newLine = chunk.indexOf('\n');
         if (newLine >= 0) {
-          req.destroy()
-          chunk = chunk.slice(0, newLine)
+          req.destroy();
+          chunk = chunk.slice(0, newLine);
         }
         if (data.length + chunk.length > maxLength) {
           if (!req.destroyed) {
-            req.destroy()
+            req.destroy();
           }
-          const error = new Error('response too large')
-          error.code = 'ELARGE'
-          return reject(error)
+          const error = new Error('response too large');
+          error.code = 'ELARGE';
+          return reject(error);
         }
-        data += chunk
-      })
+        data += chunk;
+      });
       res.on('end', () => {
         if (res.statusCode >= 400) {
-          const error = new Error(res.statusMessage)
-          error.code = res.statusCode
-          return reject(error)
+          const error = new Error(res.statusMessage);
+          error.code = res.statusCode;
+          return reject(error);
         }
 
         if (validate && validate(data)) {
-          resolve(data)
+          resolve(data);
         } else {
-          const error = new Error('invalid address')
-          error.code = 'EINVALID'
-          reject(error)
+          const error = new Error('invalid address');
+          error.code = 'EINVALID';
+          reject(error);
         }
-      })
-    })
-    req.on('error', error => reject(error))
-    req.end()
-  })
+      });
+    });
+    req.on('error', (error) => reject(error));
+    req.end();
+  });
 }
 
-module.exports = { fetchAddress, setServers, agent, lookup, DANEAgent, dns }
+module.exports = { fetchAddress, setServers, lookup, DANEAgent, dns };
